e2e2024c417545f6813a268d77043e2edbd682f8
root7325 <22177338-root-7325@users.noreply.gitlab.com>
2025-07-02 18:39:32 +0300

    refactor: merge RulesetModule into ConfigModule & protocol related modules into ProtocolModule

--- src/main/java/com/root7325/javabs/module/ProtocolModule.java
+++ src/main/java/com/root7325/javabs/module/ProtocolModule.java
@@ -1,20 +1,30 @@
 package com.root7325.javabs.module;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.TypeLiteral;
+import com.google.inject.assistedinject.FactoryModuleBuilder;
 import com.google.inject.multibindings.MapBinder;
+import com.root7325.javabs.laser.protocol.commands.CommandType;
+import com.root7325.javabs.laser.protocol.commands.LogicCommand;
+import com.root7325.javabs.laser.protocol.commands.client.LogicSetThumbnailCommand;
 import com.root7325.javabs.laser.protocol.packets.MessageType;
 import com.root7325.javabs.laser.protocol.packets.PiranhaMessage;
 import com.root7325.javabs.laser.protocol.packets.client.*;
-
-import java.util.function.Supplier;
+import com.root7325.javabs.laser.protocol.packets.ILaserServerMessageFactory;
+import com.root7325.javabs.laser.protocol.packets.server.OwnHomeDataMessage;
 
 /**
- * @author root7325 on 28.06.2025
+ * @author root7325 on 02.07.2025
  */
-public class ClientMessageModule extends AbstractModule {
+public class ProtocolModule extends AbstractModule {
     @Override
     protected void configure() {
+        configureClientMessages();
+        configureServerMessages();
+
+        configureClientCommands();
+    }
+
+    private void configureClientMessages() {
         MapBinder<MessageType, PiranhaMessage> mapBinder = MapBinder.newMapBinder(
                 binder(),
                 MessageType.class,
@@ -30,5 +40,20 @@ public class ClientMessageModule extends AbstractModule {
         mapBinder.addBinding(MessageType.GetPlayerProfile).to(GetPlayerProfileMessage.class);
         mapBinder.addBinding(MessageType.EndClientTurn).to(EndClientTurnMessage.class);
         mapBinder.addBinding(MessageType.ChangeAvatarName).to(ChangeAvatarNameMessage.class);
+    }
+
+    private void configureServerMessages() {
+        install(new FactoryModuleBuilder()
+                .implement(OwnHomeDataMessage.class, OwnHomeDataMessage.class)
+                .build(ILaserServerMessageFactory.class)
+        );
+    }
+
+    private void configureClientCommands() {
+        MapBinder<CommandType, LogicCommand> mapBinder = MapBinder.newMapBinder(
+                binder(), CommandType.class, LogicCommand.class
+        );
+
+        mapBinder.addBinding(CommandType.SetPlayerThumbnail).to(LogicSetThumbnailCommand.class);
     }
 }
